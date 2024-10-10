9daf694d0873e3c1ad4b63e1c1dac845e7c9ca24
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2024-10-10 14:22:57 +0300

    correction

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   base: '/ET-Fresh-current/frontend/', // Set the base path for GitHub Pages
   build: {
     outDir: 'dist', // Ensure the output directory is set to 'dist'
-    assetsDir: 'assets'
+    assetsDir: 'src/assets'
   },
 })
