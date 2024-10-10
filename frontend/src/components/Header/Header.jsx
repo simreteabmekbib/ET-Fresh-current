97c89144ae2ac6145012d8e5bc81a178ce8ed6de
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2024-10-10 13:26:09 +0300

    initial

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your favorite food here</h2>
+        <p>choose from a diverse menu featuring a delectable array od dishes crafted with Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum distinctio numquam quis optio veritatis itaque, asperiores, saepe deserunt aut voluptas, magnam quidem inventore accusantium mollitia exercitationem perspiciatis iure placeat? Rem.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
