7ae5849d90a9a8d89103cb33d98dcf8da09002cd
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2024-10-10 14:10:47 +0300

    correction

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ET-Fresh-current/frontend/', // Set the base path for GitHub Pages
+  build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
+  },
 })
